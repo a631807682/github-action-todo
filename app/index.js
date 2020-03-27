d993baecf485a61fcfdd30e9a53ff62950ed9291
Cr <631807682@qq.com>
2020-03-27 20:08:18 +0800

    fix: not need to unescape content.

--- app/index.js
+++ app/index.js
@@ -6,10 +6,9 @@ module.exports = async options => {
   //update to github
   let dateTime = new Date()
   let readmeContent = await buildReadmeContent({ dateTime })
-  const contentBuffer = await Buffer.from(
-    unescape(readmeContent),
-    'utf8'
-  ).toString('base64')
+  const contentBuffer = await Buffer.from(readmeContent, 'utf8').toString(
+    'base64'
+  )
   let sha = await getReadmeSHA(options)
   await updateRepositoryReadme(options, { contentBuffer, sha })
 }
